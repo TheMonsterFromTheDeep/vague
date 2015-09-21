a6d3069a4d68bddd64da7eeea650126a9e4143ea
TheMonsterFromTheDeep <bmw.notacar@gmail.com>
2015-09-21 16:02:13 -0600

    Add Dummy module
    The Dummy module is a dummy module used for constructors of Container
    classes where zero modules are passed or where the default constructor
    is called. This allows for the creation of empty containers.

--- vague/module/Container.java
+++ vague/module/Container.java
@@ -15,24 +15,41 @@ public class Container extends Module {
         for(int i = 0; i < children.length; i++) { children[i].setParent(this); }
     }
     
+    public Container() { 
+        children = new Module[] { new Dummy() }; 
+        this.setActiveChild(0); 
+        this.width = activeChild.width; 
+        this.height = activeChild.height; 
+        resize(width, height); 
+    }
+    
     public Container(Module[] children) {
-        this.children = children;
-        
-        setActiveChild(0);
-        
-        int nwidth = 0;
-        int nheight = 0;
-        for(int i = 0; i < children.length; i++) {
-            int wdif = children[i].width - (nwidth - children[i].x);
-            int hdif = children[i].height - (nheight - children[i].y);
-            if(nwidth + wdif > nwidth) { nwidth += wdif; }
-            if(nheight + hdif > nheight) { nheight += hdif; }
+        if(children.length == 0) {  
+            this.children = new Module[] { new Dummy() }; 
+            this.setActiveChild(0); 
+            this.width = activeChild.width; 
+            this.height = activeChild.height; 
+            resize(width, height); 
         }
-        this.width = nwidth;
-        this.height = nheight;
-        resize(this.width, this.height);
-        
-        initParent(); //Initialize the parent of all children as this
+        else {
+            this.children = children;
+
+            setActiveChild(0);
+
+            int nwidth = 0;
+            int nheight = 0;
+            for(int i = 0; i < children.length; i++) {
+                int wdif = children[i].width - (nwidth - children[i].x);
+                int hdif = children[i].height - (nheight - children[i].y);
+                if(nwidth + wdif > nwidth) { nwidth += wdif; }
+                if(nheight + hdif > nheight) { nheight += hdif; }
+            }
+            this.width = nwidth;
+            this.height = nheight;
+            resize(this.width, this.height);
+
+            initParent(); //Initialize the parent of all children as this       
+        }
     }
     
     @Override
@@ -52,14 +69,14 @@ public class Container extends Module {
     
     @Override
     public void mouseMove(MouseData mouseData) {
-        if(!activeChild.retainFocus) {
-            if(!activeChild.mouseInside(mouseData)) {
-                for(int i = 0; i < children.length; i++) {
-                    if(children[i].mouseInside(mouseData)) {
+        if(!activeChild.retainFocus) { //If the active child doesn't retain focus, check to see if another object is is focus
+            if(!activeChild.mouseInside(mouseData)) { //If the active child no longer contains mouse, it shouldn't necessarily be in focus
+                for(int i = 0; i < children.length; i++) { //Iterate through child modules
+                    if(children[i].mouseInside(mouseData)) { //if the mouse is inside a child module, set that one as active
                         setActiveChild(i);
                     }
                 }
-            }
+            } //If none of the modules contain the mouse, the originally active one remains active.
         }
         activeChild.mouseMove(mouseData.getShift(-x,-y,-x,-y)); //Pass the mouse data down to the active child along with the necessary shifts
     }
