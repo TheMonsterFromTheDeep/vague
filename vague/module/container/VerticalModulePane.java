5b920c6eeb6290c31d0acc09c84d3c39f5299127
TheMonsterFromTheDeep <bmw.notacar@gmail.com>
2015-09-23 16:02:24 -0600

    Make arrow graphic show when resizing
    Now an arrow graphic shows when its possible to resize and when resizing
    - I might make a different graphic depending on which it is. The resize
    threshold has also changed to make a little bit more sense in context of
    the arrow graphic.

--- vague/module/container/VerticalModulePane.java
+++ vague/module/container/VerticalModulePane.java
@@ -6,17 +6,19 @@ import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import vague.module.Module;
 import vague.module.MouseData;
+import vague.util.ImageData;
 
 public class VerticalModulePane extends Container {   
     int firstResizeIndex = -1; //Stores the index of the first module during a module resize - if no resizing is possibe, equals -1
     boolean resizeModule = false; //Stores whether a module is currently being resized
     
     boolean resizingChild = false; //Stores whether the pane is currently resizing child modules
+    boolean drawArrow = false; //Stores whether the resize arrow should be drawn
     int resizeIndex = -1; //Stores the index of the child being resized  
     
     final static Color LINE_COLOR = new Color(0xb0b3dc); //Stores the color of the lines separating modules
     final static Color VIEWPORT_COLOR = new Color(0xbfc2e7); //Stores the color of the viewport for each module
-    static final int THRESHOLD = 15; //The pixel distance from which it is possible to resize modules
+    static final int THRESHOLD = 12; //The pixel distance from which it is possible to resize modules
     static final int MIN_SIZE = 30; //Minimum size, in pixels, of child modules
     
     BufferedImage resizeControl;
@@ -62,10 +64,16 @@ public class VerticalModulePane extends Container {
     public void mouseDown(MouseEvent e) {
         if(e.getButton() == MouseEvent.BUTTON1) {
             if(!activeChild.retainFocus) {//If the active child is retaining focus, then nothing should resize
-                if(activeIndex < children.length - 1) { //If the active child is not the last, check for as first resize
-                    if(Math.abs((activeChild.y + activeChild.height) - getMouseY()) < THRESHOLD) {
+                if(activeIndex < children.length) { //If the active child is not the last, check for as first resize
+                    if(Math.abs((activeChild.y) - getMouseY()) < THRESHOLD && activeIndex > 0) {
+                        resizeIndex = activeIndex - 1;
+                        resizingChild = true;
+                        draw();
+                    }
+                    else if(Math.abs((activeChild.y + activeChild.height) - getMouseY()) < THRESHOLD && activeIndex < children.length - 1) {
                         resizeIndex = activeIndex;
                         resizingChild = true;
+                        draw();
                     }
                 }
             }
@@ -77,12 +85,15 @@ public class VerticalModulePane extends Container {
     
     @Override
     public void mouseUp(MouseEvent e) {
-        resizingChild = false;
+        if(resizingChild) {
+            resizingChild = false;
+            draw();
+        }
         activeChild.mouseUp(e);
     }
     
     @Override
-    public void mouseMove(MouseData mouseData) {
+    public void mouseMove(MouseData mouseData) {     
         if(!resizingChild) {
             if(!activeChild.retainFocus) { //If the active child doesn't retain focus, check to see if another object is is focus
                 if(!activeChild.mouseInside(mouseData)) { //If the active child no longer contains mouse, it shouldn't necessarily be in focus
@@ -90,11 +101,23 @@ public class VerticalModulePane extends Container {
                         if(children[i].mouseInside(mouseData)) { //if the mouse is inside a child module, set that one as active
                             setActiveChild(i);
                         }
-                    }
+                    }                    
                 } //If none of the modules contain the mouse, the originally active one remains active.
             }
             activeChild.mouseMove(mouseData.getShift(-x,-y,-x,-y)); //Pass the mouse data down to the active child along with the necessary shifts
-        }
+            if(Math.abs((activeChild.y) - getMouseY()) < THRESHOLD && activeIndex > 0) {
+                drawArrow = true;
+                draw();
+            }
+            else if(Math.abs((activeChild.y + activeChild.height) - getMouseY()) < THRESHOLD && activeIndex < children.length - 1) {
+                drawArrow = true;
+                draw();
+            }
+            else if(drawArrow) { //If it is true but shouldn't be, invert it and re-draw
+                drawArrow = false;
+                draw(); 
+            }
+        }       
         else {
             children[resizeIndex].resize(width, children[resizeIndex].height + mouseData.getDifY()); //Mouse moving up: module shrinks; mouse moving down: module grows
             children[resizeIndex + 1].locate(0, children[resizeIndex].y + children[resizeIndex].height + LINE_WIDTH);
@@ -124,6 +147,9 @@ public class VerticalModulePane extends Container {
         for(int i = 0; i < children.length; i++) {
             g.drawImage(children[i].lastRender, children[i].x, children[i].y, null);
             drawViewport(children[i].x, children[i].y, children[i].width, children[i].height);
+        }
+        if(drawArrow) {
+            g.drawImage(ImageData.data.resizeArrowVertical,getMouseX() - 5, getMouseY() - 16, null);
         }
     }
 }
