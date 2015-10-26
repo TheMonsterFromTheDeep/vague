1bc4dee9682837402cad56e4f82889f512bef36d
TheMonsterFromTheDeep <bmw.notacar@gmail.com>
2015-10-26 16:31:56 -0600

    Change how WorkTools are dismissed
    WorkTools are now dismissed through a red button in their upper right
    corner.

--- vague/workspace/WorkTool.java
+++ vague/workspace/WorkTool.java
@@ -19,6 +19,12 @@ public class WorkTool extends Module {
     static final Color BG_COLOR_HIGH = new Color(0xbfbfdd); //The color of the WorkTool when it is active
     static final Color BORDER_COLOR = new Color(0);
     
+    static final Color DISMISS_COLOR = new Color(0xe74f4f); //Stores the colors of the button which dismisses a WorkTool
+    static final Color DISMISS_COLOR_HIGH = new Color(0xf96666);
+   
+    private boolean closable; //Stores whether the WorkTool should dismiss on mouse down
+    //(meaning that the mouse must be located in the dismiss button)
+    
     private Module child; //The WorkTool contains a single Module child which does what it needs to do
     
     public WorkTool(Vector start, Vector end) {
@@ -59,17 +65,37 @@ public class WorkTool extends Module {
     }
     
     @Override
-    public void mouseDown(MouseEvent e) {
-        if(parent instanceof Container) {
-            ((Container)parent).removeChild(this);
+    public void mouseMove(Vector pos, Vector dif) {
+        //Check if the WorkTool is closable using so-called "magic numbers" - this checks
+        //to see if the mouse is in the top-right corner (within 20 pixels of it)
+        if(pos.x > right() - 21 && pos.x < right() && pos.y > y() && pos.y < y() + 21) {
+            closable = true;
+            redraw();
+        }
+        else {
+            if(closable) {
+                closable = false;
+                redraw();
+            }
         }
     }
     
     @Override
-    public void draw() {        
+    public void mouseDown(MouseEvent e) {
+        if(closable) {
+            if(parent instanceof Container) {
+                ((Container)parent).removeChild(this);
+            }
+        }
+    }
+    
+    @Override
+    public void draw() {     
         graphics.setColor(bgColor);
         graphics.fillRect(1, 1, width() - 2, height() - 2);
         graphics.setColor(BORDER_COLOR);
         graphics.drawRect(0,0,width() - 1,height() - 1);
+        graphics.setColor(closable ? DISMISS_COLOR_HIGH : DISMISS_COLOR); //Set the dismiss color based on whether the Module is closable
+        graphics.fillRect(width() - 21, 1, 20, 20);
     }
 }
