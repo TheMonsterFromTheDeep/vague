d2fa3bf10e7a9e9d36c3205ab2cde9c269bc7f84
TheMonsterFromTheDeep <themonsterfromthedeep@gmail.com>
2016-02-17 14:23:12 -0700

    Make color wheel work
    But I'm going to move it to a buffer, because drawing it is slow.

--- src/vague/editor/settings/ColorSetting.java
+++ src/vague/editor/settings/ColorSetting.java
@@ -6,8 +6,9 @@ import vague.menu.ToolOptions;
 
 public class ColorSetting extends ToolSetting {
     static class ColorPanel extends ToolOptions.OptionPanel {
+        
         private ColorPanel() {
-            super(100);
+            super(300);
         }
         
         public static ColorPanel create() { return new ColorPanel(); }
@@ -16,7 +17,7 @@ public class ColorSetting extends ToolSetting {
         public void paint(GraphicsHandle g) {
             g.fill(Color.BLACK);
             
-            int lcos = 50;
+            /*int lcos = 50;
             int lsin = 50;
             int ncos, nsin;
             
@@ -30,6 +31,21 @@ public class ColorSetting extends ToolSetting {
                     g.drawLine(lcos, lsin, ncos, nsin);
                     lcos = ncos;
                     lsin = nsin;
+                }
+            }*/
+            
+            int centerx = width() / 2;
+            int centery = 150;
+            int radius = 150;
+            
+            for(int x = centerx - radius; x < centerx + radius; x++) {
+                for(int y = centery - radius; y < centery + radius; y++) {
+                    float dist = (float)Math.sqrt(((x - centerx) * (x - centerx)) + ((y - centery) * (y - centery)));
+                    if(dist <= radius) {
+                        float hue = (float)(Math.atan2(y - centery, x - centerx) / (Math.PI * 2));
+                        g.setColor(new Color(Color.HSBtoRGB(hue, dist / radius, 1)));
+                        g.fillRect(x, y, 1, 1);
+                    }
                 }
             }
         }
